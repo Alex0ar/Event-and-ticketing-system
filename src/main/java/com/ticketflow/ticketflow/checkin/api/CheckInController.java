9bfe50dd0931cfbbec76c0feb99c5e7330f99f66
Alex0ar <arnautalexandru2@gmail.com>
2026-09-01 22:40:32 +0300

    check in with no endpoint

--- src/main/java/com/ticketflow/ticketflow/checkin/api/CheckInController.java
+++ src/main/java/com/ticketflow/ticketflow/checkin/api/CheckInController.java
@@ -1,11 +1,14 @@
 package com.ticketflow.ticketflow.checkin.api;
 
+import com.google.zxing.NotFoundException;
 import com.ticketflow.ticketflow.checkin.service.CheckInService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1/checkIn")
@@ -17,7 +20,8 @@ public class CheckInController {
     }
 
     @PostMapping("/{qr}")
-    public ResponseEntity<String> checkIn(@PathVariable byte[] qr) {
-        
+    public ResponseEntity<String> checkIn(@PathVariable byte[] qr) throws NotFoundException, IOException {
+        checkInService.checkIn(qr);
+        return ResponseEntity.ok("Check-in successful");
     }
 }
